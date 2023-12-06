87c037c01e091c8bdcb77fab4ee88a78669512c9
erikvella <vella.erik1@gmail.com>
2023-12-06 21:01:01 +0100

    [add] method callApi

--- app/Http/Controllers/Api/PageController.php
+++ app/Http/Controllers/Api/PageController.php
@@ -9,7 +9,7 @@ use App\Models\Project;
 class PageController extends Controller
 {
     public function index(){
-        $projects = Project::all();
+        $projects = Project::paginate(20);
         return response()->json($projects);
     }
 }
